b08a548497ca3344db80c8e0a5c5681837431c4d
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2021-10-21 11:56:38 +0600

    student page changed

--- students/index.php
+++ students/index.php
@@ -134,3 +134,39 @@ include "../api/include.php";
         </div>
 </body>
 </html>
+
+
+
+
+
+
+<!--
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add User</title>
+</head>
+<body>
+    <div class="container">
+
+        <form action="../api/add_user.php" class="col-md-3">
+            <div class="form-group">
+                <input class="form-control" type="text" placeholder="Username" name="username">
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="password" placeholder="Password" name="password">
+            </div>
+            <div class="form-group">
+                <input class="form-control" type="password" placeholder="Confirm Password" name="confirm_password">
+            </div>
+            <div class="form-group">
+                <input class="btn btn-primary" type="submit" value="Add User">
+            </div>
+        </form>
+    </div>
+</body>
+</html>
