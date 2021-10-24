53c91771e280266c65730e63b77294dfe9f62fdd
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2021-10-24 10:47:39 +0600

    folder changed

--- api/teachers/add_teacher.php
+++ api/teachers/add_teacher.php
@@ -19,7 +19,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         header("Location: ../teachers");
 
       } else {
-        echo "Error: " . $SQL . "<br>" . $mysqli->error;
+        echo $mysqli->error;
       }
 
 
