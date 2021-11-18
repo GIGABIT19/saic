ffc81ad8c2702e80cc5ec9881b68d716c812de0d
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2021-11-18 20:24:34 +0600

    formatted

--- students/index.php
+++ students/index.php
@@ -34,148 +34,107 @@ include "../api/include.php";
 </head>
 <body>
     <div class="container">
-        <!--Nav-->
+
+        <!--==================== Nav ====================-->
         <nav class="navbar navbar-inverse">
-                <div class="container-fluid">
-
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="../dashboard">SIMT</a>
-                    </div>
-                    <div class="collapse navbar-collapse" id="myNavbar">
-                        <ul class="nav navbar-nav">
-                            <li><a href="../dashboard">Dashboard</a></li>
-                            <li><a href="../teachers">Teachers</a></li>
-                            <li class="active"><a href="../students">Students</a></li>
-                            <li><a href="../departments">Departments</a></li>
-                            <li><a href="../users">Users</a></li>
-                        </ul>
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="../api/logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
-                        </ul>
-                    </div>
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="../dashboard">SIMT</a>
                 </div>
-            </nav>
-
-            <!--==================== Add Student Button ====================-->
-            <button class="btn btn-primary add-btn"><span class="glyphicon glyphicon-plus"></span> Add Student</button>
-
-            <!-- DATA Table -->
-            <div class="col-md-12">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Roll</th>
-                            <th>Name</th>
-                            <th>Phone</th>
-                            <th>Semester</th>
-                            <th>Department</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $SQL = "SELECT * FROM students";
-
-                        $result = mysqli_query($mysqli,$SQL);
-
-                        if($result->num_rows > 0){
-
-                            while($row = $result->fetch_assoc()){
-                                print "<tr>";
-                                print "<td>".$row['roll']."</td>";
-                                print "<td>".$row['name']."</td>";
-                                print "<td>".$row['phone']."</td>";
-                                print "<td>".$row['semester']."</td>";
-                                print "<td>".$row['department']."</td>";
-                                print "<td><button class='btn btn-primary view-btn'><span class='glyphicon glyphicon-eye-open'></span></button> ";
-                                print "<button class='btn btn-info edit-btn'><span class='glyphicon glyphicon-pencil'></span></button> ";
-                                print "<button class='btn btn-danger dlt-btn'><span class='glyphicon glyphicon-trash'></span></button></td>";
-                                print "</tr>";
-                            }
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav">
+                        <li><a href="../dashboard">Dashboard</a></li>
+                        <li><a href="../teachers">Teachers</a></li>
+                        <li class="active"><a href="../students">Students</a></li>
+                        <li><a href="../departments">Departments</a></li>
+                        <li><a href="../users">Users</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="../api/logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <!--==================== Add Student Button ====================-->
+        <button class="btn btn-primary add-btn"><span class="glyphicon glyphicon-plus"></span> Add Student</button>
+        
+        <!--==================== Teachers Data Table ====================-->
+        <div class="col-md-12">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Roll</th>
+                        <th>Name</th>
+                        <th>Phone</th>
+                        <th>Semester</th>
+                        <th>Department</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!--==================== Data Load ====================-->
+                    <?php
+                    $SQL = "SELECT * FROM students";
+                    $result = mysqli_query($mysqli,$SQL);
+                    if($result->num_rows > 0){
+                        while($row = $result->fetch_assoc()){
+                            print "<tr>";
+                            print "<td>".$row['roll']."</td>";
+                            print "<td>".$row['name']."</td>";
+                            print "<td>".$row['phone']."</td>";
+                            print "<td>".$row['semester']."</td>";
+                            print "<td>".$row['department']."</td>";
+                            print "<td><button class='btn btn-primary view-btn'><span class='glyphicon glyphicon-eye-open'></span></button> ";
+                            print "<button class='btn btn-info edit-btn'><span class='glyphicon glyphicon-pencil'></span></button> ";
+                            print "<button class='btn btn-danger dlt-btn'><span class='glyphicon glyphicon-trash'></span></button></td>";
+                            print "</tr>";
                         }
-
-                        ?>
-                    </tbody>
-
-                </table>
-
-            </div>
-
-            <!--==================== Add Student Modal ====================-->
-            <div class="modal fade" id="add-modal" role="dialog">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                            <h4 class="modal-title">Add Student</h4>
-                        </div>
-                        <div class="modal-body">
-                            <form action="../api/students/add_student.php" method="POST">
-                                <div class="form-group">
-                                    <input type="number" name="roll" placeholder="Roll" class="form-control" required>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" name="name" placeholder="Name" class="form-control" required>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" name="phone" placeholder="Phone" class="form-control" required>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" name="semester" placeholder="Semester" class="form-control" required>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" name="department" placeholder="Department" class="form-control" required>
-                                </div>
-                                <div class="form-group">
-                                    <input class="btn btn-primary" type="submit" value="Add Student">
-                                </div>
-                                
-                                
-                                
-                            </form>
-                        </div>
+                    }
+                    ?>
+                    
+                </tbody>
+            </table>
+        </div>
+        
+        <!--==================== Add Student Modal ====================-->
+        <div class="modal fade" id="add-modal" role="dialog">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Add Student</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form action="../api/students/add_student.php" method="POST">
+                            <div class="form-group">
+                                <input type="number" name="roll" placeholder="Roll" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="name" placeholder="Name" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="phone" placeholder="Phone" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="semester" placeholder="Semester" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" name="department" placeholder="Department" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <input class="btn btn-primary" type="submit" value="Add Student">
+                            </div>
+                        </form>
                     </div>
                 </div>
-                
             </div>
-
-
         </div>
-</body>
-</html>
-
-
-
-
-
-
-<!--
-
-
-<html lang="en">
-
-<body>
-    <div class="container">
-
-        <form action="../api/add_user.php" class="col-md-3">
-            <div class="form-group">
-                <input class="form-control" type="text" placeholder="Username" name="username">
-            </div>
-            <div class="form-group">
-                <input class="form-control" type="password" placeholder="Password" name="password">
-            </div>
-            <div class="form-group">
-                <input class="form-control" type="password" placeholder="Confirm Password" name="confirm_password">
-            </div>
-            <div class="form-group">
-                <input class="btn btn-primary" type="submit" value="Add User">
-            </div>
-        </form>
     </div>
 </body>
 </html>
