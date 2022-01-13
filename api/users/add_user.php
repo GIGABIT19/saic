9e02a189e06f3165472fe98111cf5e3078a69252
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2022-01-14 00:59:25 +0600

    update

---
+++ api/users/add_user.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+//config file
+require_once "../include.php";
+
+//Data Receiving Check
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+
+    //POSTED DATA
+    $username = $_POST['full_name'];
+    $designation = $_POST['designation'];
+    $department = $_POST['department'];
+    $phone_number = $_POST['phone_number'];
+    $email = $_POST['email'];
+
+    //SQL
+    $SQL = "INSERT INTO `teachers` (`name`,`designation`,`department`,`phone`,`email`) VALUES
+    ('$full_name','$designation','$department','$phone_number','$email')";
+
+    if ($mysqli->query($SQL) === TRUE) {
+        $_SESSION['success_msg'] = "Teacher added successfully.";
+        header("location: ../../teachers");
+      } else {
+        //failed message
+        $_SESSION['error_msg'] = $mysqli->error;
+        header("location: ../../teachers");
+      }
+
+} else {
+    header("Location: ../../teachers");
+}
--- api/users/index.php
+++ api/users/index.php
@@ -1,24 +1,36 @@
 <?php
 session_start();
+header("Content-Type:application/json");
 //TODO: Authentication
 
 
 //Config File
 require_once "../include.php";
 
-$SQL = "SELECT `id`,`username`, `active` FROM `users`";
+$SQL = "SELECT `id`,`username`,`role`,`active`,`created_at`  FROM `users`";
 
 if ($result=mysqli_query($mysqli,$SQL)){
   if ($result->num_rows > 0) {
     // output data of each row
+
+    $emparray = array();
+
     while($row = $result->fetch_assoc()) {
-      echo json_encode($row, JSON_PRETTY_PRINT);
+
+      $emparray[] = $row;
+
     }
   } else {
     echo "0 results";
   }
 
-  mysqli_free_result($result);
+  $response = [];
+
+  $response['users'] =  $emparray;
+
+  echo json_encode($response, JSON_PRETTY_PRINT);
+
+  //mysqli_free_result($result);
   
 }
 
