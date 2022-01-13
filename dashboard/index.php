d8bb59b69aa248505f4e7e84bf8a5f873cd8f868
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2022-01-13 12:47:44 +0600

    update

--- dashboard/index.php
+++ dashboard/index.php
@@ -37,7 +37,7 @@ if(!isset($_SESSION['username'])){
                             <li class="active"><a href="../dashboard">Dashboard</a></li>
                             <li><a href="../teachers">Teachers</a></li>
                             <li><a href="../students">Students</a></li>
-                            <li><a href="../departments">Summary</a></li>
+                            <li><a href="../departments">Departments</a></li>
                             <li><a href="../users">Users</a></li>
                         </ul>
                         <ul class="nav navbar-nav navbar-right">
