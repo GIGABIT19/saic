477e5f6b4e500e77a30803f6c4e48ddaaa2ffde1
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2022-01-14 00:24:05 +0600

    update api

--- api/users/index.php
+++ api/users/index.php
@@ -1,46 +1,25 @@
 <?php
-
-//DATABASE
-$SERVER = "localhost";
-$USER = "root";
-$PASS = "";
-$DB = "saic";
+session_start();
+//TODO: Authentication
 
 
-//Connection
-$mysqli = new mysqli($SERVER, $USER, $PASS, $DB);
-
-//Check Connection
-if($mysqli->connect_errno){
-    echo 'Failed to connect database:'.$mysqli->connect_error;
-}
+//Config File
+require_once "../include.php";
 
 $SQL = "SELECT `id`,`username`, `active` FROM `users`";
 
-if ($result=mysqli_query($mysqli,$SQL))
-  {
-  // Return the number of rows in result set
-  //$rowcount=mysqli_num_rows($result);
-  //printf("Result set has %d rows.\n",$rowcount);
-  // Free result set
-
-  //echo json_encode($result);
-
-
+if ($result=mysqli_query($mysqli,$SQL)){
   if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-      
-        echo json_encode($row, JSON_PRETTY_PRINT);
+      echo json_encode($row, JSON_PRETTY_PRINT);
     }
   } else {
     echo "0 results";
   }
 
-
-
   mysqli_free_result($result);
-  }
+  
+}
 
 mysqli_close($mysqli);
-?>
