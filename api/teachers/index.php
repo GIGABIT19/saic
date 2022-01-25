f16469c44d9d7ba3f6df96db13f212c259c35639
Shahed <shahed_hridoy@outlook.com>
2022-01-26 01:48:53 +0600

    file added

--- api/teachers/index.php
+++ api/teachers/index.php
@@ -1,15 +1,37 @@
 <?php
-//Session
 session_start();
+header("Content-Type:application/json");
+//TODO: Authentication
 
-//Auth
-// if(!isset($_SESSION['username'])){
-    
-// }
 
-//Include
+//Config File
 require_once "../include.php";
 
-//SQL
+$SQL = "SELECT * FROM `teachers`";
 
-//Data Shows in JSON
+if ($result=mysqli_query($mysqli,$SQL)){
+  if ($result->num_rows > 0) {
+    // output data of each row
+
+    $emparray = array();
+
+    while($row = $result->fetch_assoc()) {
+
+      $emparray[] = $row;
+
+    }
+  } else {
+    echo "0 results";
+  }
+
+  $response = [];
+
+  $response['teachers'] =  $emparray;
+
+  echo json_encode($response, JSON_PRETTY_PRINT);
+
+  //mysqli_free_result($result);
+  
+}
+
+mysqli_close($mysqli);
