162bad2fc195f75a887238f9c52522dda3673c51
Shahed <shahed_hridoy@outlook.com>
2022-01-26 00:36:55 +0600

    file uploaded

---
+++ api/students/index.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+header("Content-Type:application/json");
+//TODO: Authentication
+
+
+//Config File
+require_once "../include.php";
+
+$SQL = "SELECT * FROM `students`";
+
+if ($result=mysqli_query($mysqli,$SQL)){
+  if ($result->num_rows > 0) {
+    // output data of each row
+
+    $emparray = array();
+
+    while($row = $result->fetch_assoc()) {
+
+      $emparray[] = $row;
+
+    }
+  } else {
+    echo "0 results";
+  }
+
+  $response = [];
+
+  $response['students'] =  $emparray;
+
+  echo json_encode($response, JSON_PRETTY_PRINT);
+
+  //mysqli_free_result($result);
+  
+}
+
+mysqli_close($mysqli);
